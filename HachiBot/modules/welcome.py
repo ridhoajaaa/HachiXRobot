ceb41ab1ac18d13c355a57e3f84c8e6b1dffd69c
ddodxy <alfariziridha014@gmail.com>
2022-03-17 18:02:07 +0800

    .

--- HachiBot/modules/welcome.py
+++ HachiBot/modules/welcome.py
@@ -262,11 +262,11 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
                     bot.send_message(
                         JOIN_LOGGER,
                         f"""
-                        **#NEWGROUP**
-                        \n**Group Name:** `{chat.title}` \
-                        \n**ID:** `{chat.id}` \
-                        \n**Creator ID:** `{creator.id}` \
-                        \n**Creator Username:** @{creator.username} \
+                        <b>#NEWGROUP</b>
+                        \n<b>Group Name:</b> <code>{chat.title}</code> \
+                        \n<b>ID:</b> <code>{chat.id}</code> \
+                        \n<b>Creator ID:</b> <code>{creator.id}</code> \
+                        \n<b>Creator Username:</b> @{creator.username} \
                         """,
                         parse_mode=ParseMode.HTML,
                     )
